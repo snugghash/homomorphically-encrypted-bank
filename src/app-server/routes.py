73bc41cf2700f8d30f7ccc751712e7c59497b9b2
Suhas Gundimeda <snugghash@gmail.com>
2019-02-04 13:17:21 -0800

    app-server: Add POST route

--- src/app-server/routes.py
+++ src/app-server/routes.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+@app.route('/transact', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        amount = request.form['amount']
+        from_address = request.form['from']
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return "Bad request " + error
