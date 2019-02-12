9ae01cbe1d4bac3b786ccf3f08d9e9234d584e3f
Suhas Gundimeda <snugghash@gmail.com>
2019-02-11 17:53:36 -0800

    Fix kafka-cluster start script

--- src/data-source/ETH-blockchain/kafka-setup.sh
+++ src/data-source/ETH-blockchain/kafka-setup.sh
@@ -1,4 +1,4 @@
 kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic test
 kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic eth-old
 kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic eth-old-decrypted
-python3 kafka-producer.py
+python3 kafka_producer.py
