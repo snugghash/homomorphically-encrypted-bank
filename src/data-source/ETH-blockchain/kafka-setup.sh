b63f76eaa8f667526a46a4e3e218d93f0f8c7ad0
Ubuntu <ubuntu@ip-10-0-0-73.us-west-2.compute.internal>
2019-02-12 01:45:40 +0000

    Add topic, perms on kafka prod

--- src/data-source/ETH-blockchain/kafka-setup.sh
+++ src/data-source/ETH-blockchain/kafka-setup.sh
@@ -1,3 +1,4 @@
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic eth-old
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic test
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic eth-old
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic eth-old-decrypted
 python3 kafka-producer.py
